cd21ceb4bf9e2ca3272ff2f45d66f597ffc63266
tomohiko9090 <tomo.k9090@gmail.com>
2024-06-28 14:23:33 +0900

    fix: テストコードの修正

--- 24-6-27-blackboard.rb
+++ 24-6-27-blackboard.rb
@@ -1,3 +1,29 @@
+# 1.問題
+# https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-3-%E5%95%8F--abc-081-b---shift-only-200-%E7%82%B9
+
+# 2.ライブラリ導入
+require 'minitest/autorun'
+require 'pry'
+
+# 3.テストコード
+class TestBlackboard < Minitest::Test
+  def test_even_numbers
+    result = blackboard([16, 8, 4])
+    assert_equal [2, [4, 2, 1]], result
+  end
+
+  def test_contains_odd_number
+    result = blackboard([8, 3, 2])
+    assert_equal [0, [8, 3, 2]], result
+  end
+
+  def test_mixed_list
+    result = blackboard([16, 8, 4, 2])
+    assert_equal [1, [8, 4, 2, 1]], result
+  end
+end
+
+# 4.回答コード
 def blackboard(num_list)
   n = 0
   flag = false
--- 24-6-28-one-jadge.rb
+++ 24-6-28-one-jadge.rb
@@ -1,15 +1,38 @@
-s = "110000101"
-n = 0
+# 1.問題
+# https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-2-%E5%95%8F--abc-081-a---placing-marbles-100-%E7%82%B9
 
-s.each_char do |c|
-  if c == "1"
-    n += 1
+# 2.ライブラリ導入
+require 'minitest/autorun'
+require 'pry'
+
+# 3.テストコード
+require 'minitest/autorun'
+
+class TestOneJadge < Minitest::Test
+  def test_1
+    result = one_jadge("110000101")
+    assert_equal 4, result
+  end
+
+  def test_2
+    result = one_jadge("1")
+    assert_equal 0, result
+  end
+
+  def test_3
+    result = one_jadge("")
+    assert_equal 0, result
   end
 end
 
-class TestOneJadge < Minitest::Test
-  def test_one_jadge
-    result = one_jadge(s)
-    assert_equal n, result
+# 4.回答コード
+def one_jadge(s)
+  n = 0
+  s.each_char do |c|
+    if c == "1"
+      n += 1
+    end
   end
+
+  n
 end
