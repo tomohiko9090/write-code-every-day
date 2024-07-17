39678184e4e65d5118aaf4a8753766555b2522bb
tomohiko9090 <tomo.k9090@gmail.com>
2024-07-17 12:33:56 +0900

    今日のプログラミング

--- 24-07-16-otoshidama.rb
+++ 24-07-16-otoshidama.rb
@@ -9,11 +9,29 @@ require 'pry'
 class Test < Minitest::Test
   def test_1
     result = kane(9, 45000)
-    assert_equal (4, 0 ,5), result
+    assert_equal "4 0 5", result
   end
 end
 
 # 4.回答コード
-def kane(piece, money)
+def kane(n, y)
+  res10000 = -1
+  res5000 = -1
+  res1000 = -1
 
+  (0..n).each do |a|  # 10000円の枚数を 0 〜 N で調べる
+    (0..(n - a)).each do |b|  # 5000円の枚数を 0 〜 N-a で調べる
+      c = n - a - b  # 1000円の枚数は決まる
+      total = 10000 * a + 5000 * b + 1000 * c
+      if total == y  # 答えが見つかったら
+        res10000 = a
+        res5000 = b
+        res1000 = c
+        break
+      end
+    end
+    break if res10000 != -1  # 既に答えが見つかっている場合はループを抜ける
+  end
+
+  "#{res10000} #{res5000} #{res1000}"
 end
